13a1a2c976e60c03ab56127de5828e1b503e9ee6
Roshan Kr Soni <roshankrsoni1@gmail.com>
2020-02-18 22:51:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <main>
       <h1> 😎 Roshan Kr Soni </h1>
-   <p class="desc"> I am a full-stack web developer working on <b>ReactJs</b>, <b> ReactNative</b>, <b> MongoDB</b> & <b>Node.js</b>. Having a great passion in both back-end and front-end development,
+   <p class="desc"> I am a <b>full-stack</b> web developer working on <b>ReactJs</b>, <b> ReactNative</b>, <b> MongoDB</b> & <b>Node.js</b>. Having a great passion in both back-end and front-end development,
    I can create API's and decent UI's. I have worked on real life projects and find myself capable enough to handle pressure 
    well. <br /> Team work and quick learner describes me really well. Apart from all this I love gaming, watching anime, sports,etc. <br /> 
    I am a music lover ♫. </p>

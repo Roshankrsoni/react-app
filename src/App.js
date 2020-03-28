6ae9b11cd44ab269d6ccb866ed4c83ef7f3b55c3
Roshan Kr Soni <roshankrsoni1@gmail.com>
2020-03-29 00:03:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
    I can create API's and decent UI's. I have worked on real life projects and find myself capable enough to handle pressure 
    well. <br /> Team work and quick learner describes me really well. Apart from all this I love gaming, watching anime, sports etc. <br /> 
    I am a music lover ♫. </p>
-      
+      <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="priyanka-thecodinggirl"><a class="LI-simple-link" href='https://in.linkedin.com/in/roshankrsoni?trk=profile-badge'>Roshan Kr Soni</a></div>
     <p> 📱 +918340662031 / <b>roshankrsoni1@gmail.com </b></p> 
  
 
